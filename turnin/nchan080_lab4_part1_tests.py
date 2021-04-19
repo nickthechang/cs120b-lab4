65d53771a2a2cb4668ec58b4e159b471cb81e70a
Nicholas Chang - nchan080 <57569144+nickthechang@users.noreply.github.com>
2021-04-18 22:55:50 -0700

    Create nchan080_lab4_part1_tests.py

---
+++ turnin/nchan080_lab4_part1_tests.py
@@ -0,0 +1,12 @@
+tests = [ {'description': 'one cycle',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 1 }],
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': '2 cycle',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 1 }],
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 1 }],
+    'expected': [('PORTB',0x01)],
+    },
+    ]
+
+watch = ['PORTB']
